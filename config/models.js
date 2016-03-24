343c1fd4d99070dc1f2a9bf060c047fbdcd30a31
surajit57 <surajit@teampumpkin.com>
2016-03-24 20:38:48 +0530

    role api is added

--- config/models.js
+++ config/models.js
@@ -10,7 +10,8 @@
  */
 
 module.exports.models = {
-  connection: 'MysqlServer'
+  connection: 'MysqlServer',
+  migrate: 'safe'
 
   /***************************************************************************
   *                                                                          *
